269f677b440bbeb48922273e02947609f0357221
SanleyPan <786836525@qq.com>
2021-07-24 08:38:11 -0400

    learn pick_contact

--- app/src/main/java/com/thoughtworks/andoridtrain/LoginActivity.java
+++ app/src/main/java/com/thoughtworks/andoridtrain/LoginActivity.java
@@ -1,11 +1,16 @@
 package com.thoughtworks.andoridtrain;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         View decorView = getWindow().getDecorView();
@@ -14,5 +19,26 @@ public class LoginActivity extends AppCompatActivity {
         decorView.setSystemUiVisibility(uiOptions);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onstart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"onRestart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume");
+
     }
 }
--- app/src/main/java/com/thoughtworks/andoridtrain/MainActivity.java
+++ app/src/main/java/com/thoughtworks/andoridtrain/MainActivity.java
@@ -1,18 +1,44 @@
 package com.thoughtworks.andoridtrain;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    static final int PICK_CONTACT_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        openConstraintActivity();
+        openContact();
+
+    }
+
+    private void openContact() {
+        button = (Button) findViewById(R.id.pick_contact);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(  new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"))
+                                .addCategory(Intent.CATEGORY_DEFAULT)
+                                .setType("vnd.android.cursor.dir/phone_v2"),
+                        PICK_CONTACT_REQUEST);
+            }
+        });
+
+    }
+
+    private void openConstraintActivity() {
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +51,19 @@ public class MainActivity extends AppCompatActivity {
     private void openNewActivity() {
         Intent intent = new Intent(this, ConstraintActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_CONTACT_REQUEST && resultCode == RESULT_OK) {
+            Cursor cursor = getContentResolver().query(data.getData(), new String[]{"display_name", "data1"}, null, null, null);
+            cursor.moveToNext();
+            String contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNum = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            cursor.close();
+            Toast.makeText(this, contactName + " " + phoneNum, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
